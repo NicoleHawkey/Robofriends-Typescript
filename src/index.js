0db7fc6d2420404e43f38f39908602b1d1a12efb
Nicole Hawkey <n.laggner@gmx.at>
2023-03-14 15:00:28 +0700

    creating App.css and SearchBox, working on App.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "tachyons";
-import { robots } from './robots';
+
 
 const rootNode = document.getElementById("root");
 const root = ReactDOM.createRoot(rootNode);
 
-root.render(<CardList robots={robots}/>);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
